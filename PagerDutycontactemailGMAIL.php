4444d80a2a5d2df4fef9d91d26f4b284a6a126f6
max-otto-wm <63880637+max-otto-wm@users.noreply.github.com>
2020-05-18 17:55:16 -0300

    Add files via upload

---
+++ PagerDutycontactemailGMAIL.php
@@ -0,0 +1,16 @@
+<?php
+$emailAddress = "gomezezequielmaximiliano@gmail.com"
+
+$messages = json_decode($HTTP_RAW_POST_DATA); // alternately, try file_get_contents('php://input');
+
+if ($messages) foreach ($messages->messages as $webhook) {
+  $service = $webhook->data->incident->service->name;
+  $description = $webhook->data->incident->trigger_summary_data->subject." ".$webhook->data->incident->trigger_summary_data->description;
+  $status = $webhook->data->incident->status;
+  $subject = "$status: $description on $service";
+  $link = $webhook->data->incident->html_url;
+  $body = "$subject\n$link\n\n$HTTP_RAW_POST_DATA";
+  mail($emailAddress, $subject, $body);
+}
+//PagerDuty doesn't keep the output of this script.
+?>
